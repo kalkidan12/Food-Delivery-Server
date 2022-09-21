1d4afba087f079f5003f5c2fb5fc53d9716eb50a
kalkidan12 <kalget12@gmail.com>
2022-09-21 03:01:54 +0300

    cors

--- server.js
+++ server.js
@@ -8,27 +8,7 @@ require("dotenv").config();
 const app = express();
 
 const corsOptions = {
-	// origin: process.env.FRONTEND_BASE_URL,
-	origin: async (req, res) => {
-		res.setHeader("Access-Control-Allow-Credentials", true);
-		res.setHeader("Access-Control-Allow-Origin", "*");
-		// another common pattern
-		// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-		res.setHeader(
-			"Access-Control-Allow-Methods",
-			"GET,OPTIONS,PATCH,DELETE,POST,PUT",
-		);
-		res.setHeader(
-			"Access-Control-Allow-Headers",
-			"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-		);
-		if (req.method === "OPTIONS") {
-			res.status(200).end();
-			return;
-		}
-		return await fn(req, res);
-	},
-
+	origin: process.env.FRONTEND_BASE_URL,
 	credentials: true,
 
 	AccessControlAllowOrigin: "*",
